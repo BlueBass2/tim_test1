a56490494220ad5fb3f42051514d4fabd6a8b3d6
Paul1_Huang <156768984+BlueBass2@users.noreply.github.com>
2025-09-18 17:19:08 +0800

    Refactor Program methods to be static
    Updated methods to be static and modified CalculateBMI logic.

--- AutoTest/Program.cs
+++ AutoTest/Program.cs
@@ -14,20 +14,42 @@ namespace AutoTest
         {
         }
 
-        public float CalculateBMI()
+        public static float CalculateBMI()
         {
+            int para=0;
             float result = 0;
+
+            float height = (int)Height / 100;
+            result = Weight / (height * height);
+            result = result/0;
+
             return result;
         }
 
-        public void Save(string userInputFileName)
+        public static void Save(string userInputFileName)
         {
+            string password = "P@ssw0rd123";
+            
+            Random random = new Random();
+            int secureToken = random.Next();
+            Console.WriteLine(secureToken);
+
+            string filePath = "D:/some/directory/" + userInputFileName;
+            File.ReadAllText(filePath);
         }
 
-        public DataTable GetUserData(string userInput, SqlConnection connection)
+        public static DataTable GetUserData(string userInput, SqlConnection connection)
         {
             DataTable dt = new DataTable();
+            string query = $"select * from any.USERS where user_name = '{userInput}'";
+            
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                // 執行查詢
+            }
+            
             return dt;
         }
     }
 }
+
